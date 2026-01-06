870cd3897420f82efd06a7a2a7236022ff8ec847
dhanushd-27 <dhanushsai2005@gmail.com>
2026-01-06 21:18:27 +0530

    Add blur effect and fix border color

--- components/header/subcomponents/HeaderGetStartedButton.tsx
+++ components/header/subcomponents/HeaderGetStartedButton.tsx
@@ -4,14 +4,12 @@ import React from "react";
 
 import { ContainedButton } from "@/components/ui/buttons";
 
-type HeaderGetStartedButtonProps = {};
-
 /**
  * HeaderGetStartedButton
  * ----
  * Renders the primary CTA for navigating to the dashboard.
  */
-export const HeaderGetStartedButton: React.FC<HeaderGetStartedButtonProps> = () => {
+export const HeaderGetStartedButton = () => {
   return <ContainedButton href="/dashboard">Get started</ContainedButton>;
 };
 
--- components/header/subcomponents/HeaderLogo.tsx
+++ components/header/subcomponents/HeaderLogo.tsx
@@ -2,18 +2,17 @@
 
 import React from "react";
 import Link from "next/link";
-
-type HeaderLogoProps = {};
+import Image from "next/image";
 
 /**
  * HeaderLogo
  * ----
  * Renders the Waku brand mark and routes to the top-of-page anchor.
  */
-export const HeaderLogo: React.FC<HeaderLogoProps> = () => {
+export const HeaderLogo = () => {
   return (
-    <Link className="inline-flex items-center" href="#home">
-      <img src="/waku.png" alt="Waku Logo" className="h-8 w-auto" />
+    <Link className="inline-flex items-center" href="/">
+      <Image alt="Waku Logo" className="h-8 w-auto" height={32} src="/waku.png" width={128} />
     </Link>
   );
 };
